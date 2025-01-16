9c9e13077d0baa5e6f616a054511dd9d41cc61f3
ghazzor <kartikhis8o@gmail.com>
2025-01-16 20:12:28 +0530

    try building twrp14

--- device.mk
+++ device.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # define hardware platform
 PRODUCT_PLATFORM := holi
